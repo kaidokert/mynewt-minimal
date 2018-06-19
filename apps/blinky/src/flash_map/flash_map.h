b9ada6cad03207ff0ae4e941dd939e6b9f0ad4c5
Kaido <kaido@myblossom.com>
2018-06-18 18:10:08 -0700

    Point fakeflash at real one

--- apps/blinky/src/flash_map/flash_map.h
+++ apps/blinky/src/flash_map/flash_map.h
@@ -1,14 +1,6 @@
 #ifndef FAKE_FLASH
 #define FAKE_FLASH
 
-#include <stdint.h>
-
-struct flash_area {
-    uint8_t fa_id;
-    uint8_t fa_device_id;
-    uint16_t pad16;
-    uint32_t fa_off;
-    uint32_t fa_size;
-};
+#include <../../flash_map/include/flash_map/flash_map.h>
 
 #endif
